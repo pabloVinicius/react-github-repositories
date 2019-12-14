219c0305d6c6c5bcca55d10db5e3f5b04dacc471
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-14 10:31:41 -0300

    requesting repository details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 const Repository = ({ match }) => {
+  const [repository, changeRepository] = useState({});
+  const [issues, changeIssues] = useState({});
+  const [loading, changeLoading] = useState(true);
+
+  const getRepoDetails = async () => {
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repositoryData, issuesData] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    changeRepository(repositoryData);
+    changeIssues(issuesData);
+    changeLoading(false);
+  };
+
+  useEffect(() => {
+    getRepoDetails();
+  }, []);
+
   return <div>Repository: {decodeURIComponent(match.params.repository)}</div>;
 };
 
