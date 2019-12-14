c6a570721742ce4d76f7114492c65d03d33a2939
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-14 10:08:06 -0300

    connecting add repository flow to github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-const index = () => {
-  return <div>Main</div>;
+import api from '../../services/api';
+
+const Main = () => {
+  const [loading, changeLoading] = useState(false);
+  const [newRepo, changeNewRepo] = useState('');
+  const [repositories, changeRepositories] = useState([]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    changeLoading(true);
+
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    changeRepositories([...repositories, data]);
+    changeLoading(false);
+    changeNewRepo('');
+  };
+
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          value={newRepo}
+          onChange={e => changeNewRepo(e.target.value)}
+          type="text"
+          placeholder="Adicionar respositório"
+        />
+
+        <SubmitButton type="submit" loading={loading}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
-export default index;
+export default Main;
