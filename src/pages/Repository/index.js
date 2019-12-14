e0eb83dbb6d41a0be814e942e004a051358e09e6
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-14 14:59:50 -0300

    showing repository issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import Container from '../../components/Container';
 
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 const Repository = ({ match }) => {
   const [repository, changeRepository] = useState({});
@@ -43,6 +43,22 @@ const Repository = ({ match }) => {
         <h1>{repository.name}</h1>
         <p>{repository.description}</p>
       </Owner>
+      <IssueList>
+        {issues.map(issue => (
+          <li key={`${issue.id}`}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label => (
+                  <span key={`${label.id}`}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
     </Container>
   );
 };
